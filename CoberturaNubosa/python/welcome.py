bf9e2d5b9d2995d21d4393951eac7fd7d13e852c
Jose Antonio Barrientos Sanchez <a.barrientos@ciencias.unam.mx>
2023-11-07 18:16:08 -0600

    Manejador de Entrada completado
    Se realizo la estructura completa del archivo manejador entrada. Sugerencia de dividir el proyecto en paquetes.

--- CoberturaNubosa/python/welcome.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-player = input("I want your age: \n")
-age = int(player)
-
-if age < 18:
-    sys.exit("You are too young to play this game.")
-else:
-    print("Welcome to the game!")
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+
+from ManejadorEntrada import verificar_imagen, ErrorFormato, ErrorDimension, manejar_entrada
+
 def main():
     """
     Funcion principal del programa.
@@ -12,7 +16,17 @@ def main():
     largo_imagen = 2912
     centro_circulo = (2184,1456)
     radio_circulo = 1324
-
+    nombre_imagen, bandera_s = manejar_entrada()
+    if nombre_imagen is None:
+        return
+    try:
+        verificar_imagen(nombre_imagen, ancho_imagen, largo_imagen)
+    except ErrorFormato as e:
+        print(str(e))
+        return
+    except ErrorDimension as e:
+        print(str(e))
+        return
 
 if __name__ == "__main__":
     main()
